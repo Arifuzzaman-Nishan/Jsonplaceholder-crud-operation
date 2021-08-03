36820ed3b73264a3a6274d0f140a7c69fc5fa222
Arifuzzaman-Nishan <arifuzzamannishan@gmail.com>
2021-08-04 04:14:05 +0600

    update and delete added

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-unused-vars */
 import axios from 'axios';
 import MaterialTable from 'material-table';
 import React, { useEffect, useState } from 'react';
@@ -27,6 +29,7 @@ export default function Home() {
     ];
 
     const [userData, setUserData] = useState([]);
+    // const [filterData, setFilterData] = useState([]);
 
     // user api fetch
 
@@ -43,11 +46,65 @@ export default function Home() {
 
     console.log(userData);
 
+    // const filterData = [];
+
+    // userData.forEach((element) => {
+    //     filterData.push({
+    //         name: element.name,
+    //         username: element.username,
+    //         email: element.email,
+    //         phone: element.phone,
+    //         website: element.website,
+    //     });
+    // });
+
+    // console.log(filterData);
+
     return (
         <div>
             <h1>This is home component</h1>
-            <div style={{ maxWidth: '90%' }}>
-                <MaterialTable title="User Information" columns={columnData} data={userData} />
+            <div style={{ maxWidth: '100%' }}>
+                <MaterialTable
+                    title="User Information"
+                    columns={columnData}
+                    data={userData}
+                    editable={{
+                        onRowAdd: (newData) =>
+                            axios
+                                .post('https://jsonplaceholder.typicode.com/users', newData)
+                                .then((res) => setUserData([...userData, newData])),
+
+                        onRowUpdate: (newData, oldData) =>
+                            axios
+                                .patch(
+                                    `https://jsonplaceholder.typicode.com/users/${
+                                        oldData.tableData.id + 1
+                                    }`,
+                                    newData
+                                )
+                                .then((res) => {
+                                    console.log(res.data);
+                                    const dataUpdate = [...userData];
+                                    const index = oldData.tableData.id;
+                                    dataUpdate[index] = res.data;
+                                    setUserData(dataUpdate);
+                                }),
+
+                        onRowDelete: (oldData) =>
+                            axios
+                                .delete(
+                                    `https://jsonplaceholder.typicode.com/users/${
+                                        oldData.tableData.id + 1
+                                    }`
+                                )
+                                .then((res) => {
+                                    const dataDelete = [...userData];
+                                    const index = oldData.tableData.id;
+                                    dataDelete.splice(index, 1);
+                                    setUserData([...dataDelete]);
+                                }),
+                    }}
+                />
             </div>
         </div>
     );
